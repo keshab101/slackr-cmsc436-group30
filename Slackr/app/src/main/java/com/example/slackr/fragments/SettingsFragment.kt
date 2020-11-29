aa80ffe5b26d262f11c2ebe7cc66a91ea3052d40
Nahom Tesfatsion <nahomhaile72@gmail.com>
2020-11-28 19:52:23 -0500

    Added profile

--- Slackr/app/src/main/java/com/example/slackr/fragments/SettingsFragment.kt
+++ Slackr/app/src/main/java/com/example/slackr/fragments/SettingsFragment.kt
@@ -13,7 +13,7 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.profile, container, false)
     }
 
 }
