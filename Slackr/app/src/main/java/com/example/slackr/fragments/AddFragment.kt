21112d1f60f11c0d177c10bc4eee3505b2cbf560
Nahom Tesfatsion <nahomhaile72@gmail.com>
2020-12-05 10:55:50 -0500

    Made basic create group UI or layout

--- Slackr/app/src/main/java/com/example/slackr/fragments/AddFragment.kt
+++ Slackr/app/src/main/java/com/example/slackr/fragments/AddFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import com.example.slackr.R
 
 
@@ -14,7 +16,25 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_add, container, false)
+
+        val subjects = resources.getStringArray(R.array.subjects)
+        val editText: AutoCompleteTextView = view.findViewById(R.id.group_subject)
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            activity!!.applicationContext,
+            R.layout.cg_custom_subject_item, R.id.text_view_list_item, subjects
+        )
+        editText.setAdapter(adapter)
+        //editText.setThreshold(1);
+        //get the input like for a normal EditText
+        //String input = editText.getText().toString();
+
+
+        return view;
+
+
     }
+
 
 }
