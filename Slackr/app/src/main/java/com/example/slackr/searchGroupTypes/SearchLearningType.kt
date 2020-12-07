e28b801a7ad66fb4572a0f8d65b0ef779e37f340
Nahom Tesfatsion <nahomhaile72@gmail.com>
2020-12-06 20:46:55 -0500

    adding search by learning type

--- Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchLearningType.kt
+++ Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchLearningType.kt
@@ -1,12 +1,20 @@
 package com.example.slackr.searchGroupTypes
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import com.example.slackr.R
 
 class SearchLearningType: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.)
 
     }
+
+
 }
