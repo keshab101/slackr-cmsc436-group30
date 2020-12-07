918a62678b0a97c2074abb922b22bb1a14530248
Keshab Acharya <acharyakeshab1997@gmail.com>
2020-12-06 22:09:37 -0500

    Modified search names

--- Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchDayTime.kt
+++ Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchDayTime.kt
@@ -6,6 +6,8 @@ import com.example.slackr.R
 
 class SearchDayTime: AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_group_by_studytime)
