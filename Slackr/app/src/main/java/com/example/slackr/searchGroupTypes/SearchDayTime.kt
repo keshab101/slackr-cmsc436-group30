df9344e3ea1461e9be4f8e7f5a58a8a535fb9d1b
Keshab Acharya <acharyakeshab1997@gmail.com>
2020-12-06 22:10:16 -0500

    Search names for layouts

--- Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchDayTime.kt
+++ Slackr/app/src/main/java/com/example/slackr/searchGroupTypes/SearchDayTime.kt
@@ -1,18 +1,19 @@
 package com.example.slackr.searchGroupTypes
 
 import android.os.Bundle
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.slackr.R
 
 class SearchDayTime: AppCompatActivity() {
 
-
+    private var studyTime: RadioGroup? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.search_group_by_studytime)
-
-
+        
+        //studyTime = findViewById(R.id.)
 
     }
 
